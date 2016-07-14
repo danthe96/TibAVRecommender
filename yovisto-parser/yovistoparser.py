1e9a328b182a7c5a4d6cedbc22f9fc955e198b44
Nils Thamm <nils.thamm@student.hpi.de>
2016-07-14 13:02:20 +0200

    added yovisto dbpedia mapping

--- yovisto-parser/yovistoparser.py
+++ yovisto-parser/yovistoparser.py
@@ -1,17 +1,39 @@
-import feedparser
-from pyRdfa import pyRdfa
+# from pyRdfa import pyRdfa
+# from pyRdfa.options import Options
+# import xml.etree.ElementTree
 
-python_wiki_rss_url = "http://blog.yovisto.com/feed/"
-feed = feedparser.parse( python_wiki_rss_url )
-entries =  feed[ "items" ] 
-sources = []
 
-f = open('yovistoextract.ttl', 'w')
+# options = Options(vocab_expansion=True)
+# sources = []
 
-for entry in entries:
-	sources.append(entry["link"])
+# e = xml.etree.ElementTree.parse('post-sitemap2.xml').getroot()
 
-for source in sources:
-	f.write(pyRdfa().rdf_from_source(source))
+# print(e)
 
+# for aurl in e:
+#     sources.append(aurl[0].text)
+
+f = open('yovistoextract.txt', 'r+')
+f_out = open('yovistoextract_dbpedia.txt', 'w')
+
+# i = 1
+
+# for source in sources:
+# 	nt = pyRdfa(options=options).rdf_from_source(source, outputFormat='nt')
+# 	f.write(nt)
+# 	print("finished " + str(i) + " of " + str(len(sources)))
+# 	i += 1
+
+for line in f:
+	if '<http://www.w3.org/2004/02/skos/core#subject> <http://dbpedia.org/resource' not in line:
+		continue
+	print(line[:-2])
+	f_out.write(line[:-2] + '\n')
+
+
+
+#nt = pyRdfa(options=options).rdf_from_source('http://blog.yovisto.com/roger-cotes-and-newtons-principia-mathematica/', outputFormat='nt')
+#print(nt)
+#f.write(nt)
 f.close()
+f_out.close()
